5df107557292c981baba975692fb0ccfb4b6007d
Mohammed Ali <darthmo72@gmail.com>
2013-05-07 17:10:53 +0100

    Files Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AthleteBBC::Application.config.secret_token = '65d1897e8fae3bb93855e85e8a1a40eda2318f1f3349cd8be9d990938644acc2ff9d2c699c054b5d815122d383989179cdbe1bf07663286c5edea5d09b1cb1f6'
