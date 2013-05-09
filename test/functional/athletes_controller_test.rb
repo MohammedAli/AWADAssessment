eb43fa66cbaf3dab6ff47422d364bfafa890618c
Mohammed Ali <darthmo72@gmail.com>
2013-05-09 23:50:27 +0100

    Model Testing

--- test/functional/athletes_controller_test.rb
+++ test/functional/athletes_controller_test.rb
@@ -6,9 +6,7 @@ class AthletesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
+  # test "should get show" do  #   get :show
+  #   assert_response :success
+  # end
 
-end
--- test/unit/star_test.rb
+++ test/unit/star_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class StarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    def test_uniqueness_for_star_fields
+    star = Star.create(:athlete_id => 1, :user_id => 1)
+    star1 = Star.create(:athlete_id => 1, :user_id => 1)
+    assert !star1.valid?, "One user can't star one athlete more than one time"
+  end
 end
